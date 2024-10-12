bedad317f6031fd918d68e73b0c636f08f1bb136
shiyizhiyang <2569805170@qq.com>
2024-10-12 13:33:25 +0800

    pre test

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+from torch import nn
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+print(f"Pytorch version:{torch.__version__}\n torchvision version:{torchvision.__version__}")
