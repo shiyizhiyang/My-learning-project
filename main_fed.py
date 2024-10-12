51598fb9b4a3bb5e73d9c8553667045a62350085
shiyizhiyang <2569805170@qq.com>
2024-10-12 11:08:18 +0800

    add data

--- main_fed.py
+++ main_fed.py
@@ -10,12 +10,14 @@ import numpy as np
 from torchvision import datasets, transforms
 import torch
 
+
 from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
 from utils.options import args_parser
 from models.Update import LocalUpdate
 from models.Nets import MLP, CNNMnist, CNNCifar
 from models.Fed import FedAvg
 from models.test import test_img
+
 
 
 if __name__ == '__main__':
